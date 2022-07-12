794ebdf175f4257fba3dc2b150d96cc21e60c28e
Oleg Kotov <kotov.oleg1202@gmail.com>
2022-07-12 11:25:14 +0300

    Client

---
+++ Back/client1/src/pages/Events.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Events = () => {
+    return (
+        <div>
+            Почему не работает!
+        </div>
+    );
+};
+
+export default Events;
---
+++ Back/client1/src/routes.js
@@ -0,0 +1,32 @@
+// import Admin from "./pages/Admin"
+// import Auth from "./pages/Auth"
+// import EventPage from "./pages/EventPage"
+// import Events from "./pages/Events"
+// import { ADMIN_ROUTE, EVENTPAGE_ROUTE, EVENTS_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE } from "./utils/consts"
+
+
+// export const authRoutes = [
+//   {
+//     path: ADMIN_ROUTE,
+//     Component: Admin
+//   },
+// ]
+
+// export const publicRoutes = [
+//     {
+//       path: EVENTS_ROUTE,
+//       Component: Events
+//     },
+//     {
+//       path: REGISTRATION_ROUTE,
+//       Component: Auth
+//     },
+//     {
+//       path: LOGIN_ROUTE,
+//       Component: Auth
+//     },
+//     {
+//       path: EVENTPAGE_ROUTE + '/:id',
+//       Component: EventPage
+//     },
+// ]
